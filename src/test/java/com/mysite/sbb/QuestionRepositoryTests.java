3a3d562cdf179a286c10d3b1045689fe63182eac
Jung Yunseo <yunseo2488@naver.com>
2025-08-27 15:09:51 +0900

    데이터 수정 추가

--- src/test/java/com/mysite/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/mysite/sbb/QuestionRepositoryTests.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -64,5 +65,19 @@ class QuestionRepositoryTests {
 		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("데이터 수정")
+	void t6() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+
+		this.questionRepository.save(q);
+
+		Question q2 = this.questionRepository.findById(1).get();
+		assertEquals("수정된 제목", q2.getSubject());
 	}
 }
