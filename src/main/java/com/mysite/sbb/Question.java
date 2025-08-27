a520863fd7f6ac2992545bc9ecc122c7e823d934
Jung Yunseo <yunseo2488@naver.com>
2025-08-27 12:19:34 +0900

    특정 글에 관련된 질문 조회

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -1,10 +1,17 @@
 package com.mysite.sbb;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+// entity 때문에 question 은 table 이 되어야함
+
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +24,14 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+    // 클래스간의 힌트 주기 -> @OneToMany
+    // 얘는 Column 이 안 생김!
+    // 왜냐? 리스트는 여러 개의 값을 갖고 있는데 column 안에는
+    // 하나의 값 밖에 넣을 수가 없다!
+    // Question_Answers 라는 새로운 테이블을 만들어 해결해줌
+    // 따라서 관계만 잘 설정하면 된다
+    @OneToMany
+    private List<Answer> answers;
 
 }
--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -1,13 +1,33 @@
 package com.mysite.sbb;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.desktop.QuitEvent;
+import java.util.List;
+
 @SpringBootApplication
+
 public class SbbApplication {
 
 	public static void main(String[] args) {
+
+		/*
+		Answer a = new Answer();
+
+		// 답글로부터 질문 찾아감
+		Question question = a.getQuestion();
+		question.getContent();
+
+		// 질문 조회
+		Question q = new Question();
+		List<Answer> answers = q.getAnswers();
+
 		SpringApplication.run(SbbApplication.class, args);
+		 */
 	}
 
 }
