3b58483e9b2c576298056a4d246711e3c7ac58f8
Jung Yunseo <yunseo2488@naver.com>
2025-08-27 11:08:43 +0900

    @responsebody 추가

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,17 +2,20 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // controller 임을 알려줘야함
 public class MainController {
     // 함수 생성 - 기능
     @GetMapping("/")
-    public void index() {
-        System.out.println("index() 메서드 호출됨!");
+    @ResponseBody // 리턴값을 브라우저에 보여준다
+    public String index() {
+        return "sbb";
     }
 
     @GetMapping("/hello") // /hello 라고 입력하면 해당 메서드 호출 가능
-    public void hello() {
-        System.out.println("hello() 메서드 호출됨!");
+    @ResponseBody
+    public String hello() {
+        return "hello";
     }
 }
