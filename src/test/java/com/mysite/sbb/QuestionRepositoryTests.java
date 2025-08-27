88424bc8bdce357e376e8c274b22492d392e74e4
Jung Yunseo <yunseo2488@naver.com>
2025-08-27 14:26:04 +0900

    findById 테스트 케이스 추가

--- src/test/java/com/mysite/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/mysite/sbb/QuestionRepositoryTests.java
@@ -1,28 +1,44 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class SbbApplicationTests {
+class QuestionRepositoryTests {
 
 	@Autowired // 테스트 환경에서 필수
 	private QuestionRepository questionRepository;
 
 	@Test
+	@DisplayName("findAll")
 	void t1() {
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("findById")
+	void t2() {
+		// findById -> Optional 로 가져온다
+		Optional<Question> oq = this.questionRepository.findById(1);
+
+		// 존재하는지 체크 -> 나오면 true
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 
 }
